0a9cba07dc5a2475dc669c4fad8b704ab37c4c6d
Alex Braga Rodrigues <6666alex@gmail.com>
2017-11-05 11:33:57 -0200

    Commit Models

---
+++ tcc_admin/app/Entregador.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lexbr
+ * Date: 05/11/2017
+ * Time: 01:17
+ */
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Entregador extends Model
+{
+    //Acessor : permite que você defina um atributo personalizado no modelo do Eloquent para quando voce estiver lendo dados
+    //-Estrutura: get{NomaDaPropriedade}Attribute $cliente->nome_da_propriedade
+
+}
